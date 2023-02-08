b6bf7e9e6dc0dd802b0d732c832393b065543eae
Steff Potter <steff@hopeandpotter.co.uk>
2023-02-08 21:32:28 +0000

    Added comments to exercise 12 code

--- DevOps_Exercises/12_read_file.py
+++ DevOps_Exercises/12_read_file.py
@@ -1,12 +1,22 @@
-
+# open the file and read in the contents
 pelican = open("pelican.txt").read()
+
+# print the object type returned by the read() method (str)
 print(type(pelican))
+
+# print the entire contents of the file
 print(pelican)
 
+# open the file and read the contents into a list
 pelican_list = open("pelican.txt").readlines()
+
+# print the object type returned by the readlines() method (list)
 print(type(pelican_list))
+
+# get the number of objects in the list using the len() function and print the result
 print(len(pelican_list))
 
-#
+# iterate through pelican_list and print each line to the console
+# using the end parameter to specify that we don't want a new line created at the end of each print statement
 for line in pelican_list:
     print(line, end="")
--- DevOps_Exercises/12_write_file.py
+++ DevOps_Exercises/12_write_file.py
@@ -1,10 +1,17 @@
 # open the file if it exists, if it doesn't create it
+# switch "w" truncates the file contents (so clears the file) and then appends
+# switch "a" will append to what is already in the file
 poem = open("pelican.txt", "w")
 
+# write the first two lines of the poem to the txt file
+# the write method takes in a single object
 poem.write("A wonderful bird is the pelican\n")
 poem.write("His bill holds more than his belican\n")
 
+# add the remaining lines of the poem to a list
 lines = ["He can take in his beak,\n", "Enough food for a week,\n", "But I'm damned if I can see how the helican\n"]
 
+# write the lines of the list to the file
+# the writelines method takes in a collection of objects
 poem.writelines(lines)
 
